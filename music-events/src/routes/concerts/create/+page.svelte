721143106b1439e5445824ee6451123fc3a697a4
alordash <alordash@mail.ru>
2023-05-05 01:57:27 +0300

    Simplified create page

--- music-events/src/routes/concerts/create/+page.svelte
+++ music-events/src/routes/concerts/create/+page.svelte
@@ -2,14 +2,8 @@
 	import { createEmptyConcert, addConcert } from '$lib/model/concert/Concert';
 	import ConcertObjectCreate from '$lib/model/concert/ConcertCreate.svelte';
 	let concert = createEmptyConcert();
-
-	async function onCreate() {
-		let id = await addConcert(concert);
-		console.log(`concert :>> ${concert}`, concert);
-		return id;
-	}
 </script>
 
 <div class="w-50">
-	<ConcertObjectCreate {concert} createCallback={onCreate} />
+	<ConcertObjectCreate {concert} createCallback={addConcert} />
 </div>
