cad4ca94f0934b6ed4ff760ea2112f3235324533
alordash <alordash@mail.ru>
2023-05-11 16:01:49 +0300

    FIXING TIMER

--- music-events/src/lib/timer.ts
+++
@@ -1,7 +0,0 @@
-export async function sleep(ms: number) {
-    await new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export async function sleepMaxOneSec() {
-    await sleep(Math.random() * 500 + 200);
-}
