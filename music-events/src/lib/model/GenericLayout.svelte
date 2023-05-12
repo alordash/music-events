8d277ec1876e640eab0639da0e15dacfd000cfa2
alordash <alordash@mail.ru>
2023-05-12 13:29:01 +0300

    Added generic layout

---
+++ music-events/src/lib/model/GenericLayout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const currentUrl = $page.url.toString();
+	const loadUrl = currentUrl.substring(0, currentUrl.lastIndexOf('/')) + '/load';
+	const createUrl = currentUrl.substring(0, currentUrl.lastIndexOf('/')) + '/create';
+</script>
+
+<ul class="nav nav-tabs">
+	<li class="nav-item">
+		<a id="loadTab" class="nav-link active" aria-current="page" href={loadUrl}>Load</a>
+	</li>
+	<li class="nav-item">
+		<a id="createTab" class="nav-link active" aria-current="page" href={createUrl}>Create</a>
+	</li>
+</ul>
